e50a0469b3adcf3c6a9b1054582ce9b104f0e4ff
Nelthi <nelson.tiozang@facsciences-uy1.cm>
2026-04-21 23:42:55 +0100

    Update database URI and secret key in config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql://neondb_owner:npg_epmB3rX6KuaD@ep-empty-mountain-a4sjb3xc-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'une_cle_par_defaut')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'azerty')
